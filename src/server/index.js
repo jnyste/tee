96135c6b273c13f66a9765278cf391c712b51dff
Jan-Sebastian Nystedt <jan-sebastian.nystedt@cs.tamk.fi>
2018-11-09 19:21:51 +0200

    .

--- src/server/index.js
+++ src/server/index.js
@@ -6,14 +6,16 @@ const app = express();
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 app.get('/api/pullFromGit', function(req, res) {
+  let pull = shell.exec('git pull');
+  let output = pull.stdout;
   switch (code = shell.exec('git pull').code) {
     case 0:
-      res.send('Git pull successful!');
+      res.send(output);
       break;
     case 1:
     case 2:
     default:
-      res.send('Git pull failed?' + code);
+      res.send(output);
       break;
   }
 });
