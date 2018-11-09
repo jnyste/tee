af190ba920c8bc4c4872e8e82903c28f2fcd51e8
Jan-Sebastian Nystedt <jan-sebastian.nystedt@cs.tamk.fi>
2018-11-09 19:48:47 +0200

    .

--- src/client/ListItem.js
+++ src/client/ListItem.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 export default class ListItem extends Component {
     
