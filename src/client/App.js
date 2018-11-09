49514612fba530ee8abd2d39e465c61fba984f94
Jan-Sebastian Nystedt <jan-sebastian.nystedt@cs.tamk.fi>
2018-11-09 19:45:04 +0200

    .

--- src/client/App.js
+++ src/client/App.js
@@ -15,7 +15,7 @@ export default class App extends Component {
   render() {
     const { username } = this.state;
     return (
-      <Grid container column>
+      <Grid container direction="column">
         <ListItem text="Ping www.google.com"/>
         <ListItem text="Play more Eurobeat"/>
         <ListItem text="Juo es :D "/>
