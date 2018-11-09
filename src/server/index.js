16317bd88c0a0f3dea55a853a5c2fff87dca240f
Jan-Sebastian Nystedt <jan-sebastian.nystedt@cs.tamk.fi>
2018-11-09 16:51:31 +0000

    this is an abomination

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const os = require('os');
-
+const shell = require('shelljs');
 const app = express();
 
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+app.get('/api/pullFromGit', function(req, res) {
+  switch (code = shell.exec('git pull').code) {
+    case 0:
+      res.send('Git pull successful!');
+      break;
+    case 1:
+    case 2:
+    default:
+      res.send('Git pull failed?' + code);
+      break;
+  }
+});
 app.listen(8080, () => console.log('Listening on port 8080!'));
