b85fcfcda3b43f8c52730a614d63d780baeadce8
Jan-Sebastian Nystedt <jan-sebastian.nystedt@cs.tamk.fi>
2018-11-09 22:13:13 +0200

    .

--- src/client/App.js
+++ src/client/App.js
@@ -18,7 +18,11 @@ const styles = theme => ({
       marginLeft: 'auto',
       marginRight: 'auto',
     } */
-}});
+},
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing.unit * 3
+    }});
 
 function App(props) {
   const { classes } = props;
@@ -29,12 +33,24 @@ function App(props) {
       <div className={classes.layout}>
       <CategoryDrawer />
       <MenuBar/>
-        <Grid container direction="column" alignItems="stretch" spacing={16}>
+        {/* <Grid container direction="column" alignItems="stretch" spacing={16}>
             <ListItem text="Ping www.google.com"/>
             <ListItem text="Ping www.google.com again"/>
             <ListItem text="Do something else cool"/>
-        </Grid>
+        </Grid> */}
       </div>
+      <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+          donec massa sapien faucibus et molestie ac.
+        </Typography>
     </React.Fragment>
   );
 }
