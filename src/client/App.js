a92d2c2cb96dac6bf956d11d7f1c7396f13dae43
Jan-Sebastian Nystedt <jan-sebastian.nystedt@cs.tamk.fi>
2018-11-09 20:48:34 +0200

    .

--- src/client/App.js
+++ src/client/App.js
@@ -1,47 +1,42 @@
-import React, { Component } from 'react';
-import './app.css';
-import ListItem from './ListItem.js';
-import Grid from '@material-ui/core/Grid';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Hidden from '@material-ui/core/Hidden';
+import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
 
-class App extends Component {
+const styles = theme => ({
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
+      width: 1100,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  }});
 
-  state = { username: null };
+function App(props) {
+  const { classes } = props;
 
-  constructor(props) {
-      const { classes } = this.props;
-      
-      const styles = theme => ({
-        layout: {
-            width: 'auto',
-            marginLeft: theme.spacing.unit * 3,
-            marginRight: theme.spacing.unit * 3,
-            [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
-              width: 1100,
-              marginLeft: 'auto',
-              marginRight: 'auto',
-            }
-        }
-      });
-  }
-
-  componentDidMount() {
-    /* fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username })); */
-  }
-
-  render() {
-    const { username } = this.state;
-    return (
-        <React.Fragment>
-        <CssBaseline/>
-        <div className={classes.layout}>
-        </div>  
-        </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <div className={classes.layout}>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default withStyles(styles)(App);
