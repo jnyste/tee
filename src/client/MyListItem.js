a287a5a77fcc28b4e00cc9e951106dfac0a22b56
salonjul <julius.salonen@visma.com>
2018-11-10 13:42:13 +0200

    list item colors

--- src/client/MyListItem.js
+++ src/client/MyListItem.js
@@ -29,7 +29,7 @@ const styles = theme => ({
     paddingTop: theme.spacing.unit * 2,
     paddingBottom: theme.spacing.unit * 2,
     paddingLeft: theme.spacing.unit * 2,
-    background: 'linear-gradient(30deg, #C16B6B 85%, #836a6a 85%)'
+    background: 'linear-gradient(-30deg, #C16B6B 85%, #b46a6a 85%)'
   }
 });
 
