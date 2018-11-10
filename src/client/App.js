6b7c142b3b683ff889c6c528cbd87e4509144802
Jan-Sebastian Nystedt <jan-sebastian.nystedt@cs.tamk.fi>
2018-11-10 12:25:31 +0200

    .

--- src/client/App.js
+++ src/client/App.js
@@ -173,7 +173,7 @@ class App extends React.Component {
             }}
             anchor="left"
             >
-            <img src="public/tee.png" height="50" width="200" style={{marginTop: "10px", marginLeft: "10px"}}/>
+            <img src="public/tee.png" height="100" width="220" style={{marginTop: "5px", marginLeft: "20px"}}/>
             <div className={this.props.classes.toolbar} />  
             <Divider />
             <List>
